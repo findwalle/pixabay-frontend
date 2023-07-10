9565bcec55d7cf8f0ddf325f9ffb96dc364e967e
Walter Li <li.walter1@gmail.com>
2023-07-10 18:05:00 -0400

    updating modal

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -6,10 +6,10 @@ function ImageModal({ isOpen, setClose, details }) {
   // TODO: address userImageURL not loading
 
   return (
-    <div className="fixed inset-0 z-10 flex items-center justify-center">
+    <div className="fixed inset-0 z-10 flex flex-col items-center justify-center bg-slate-100">
       <button className="h-500 w-500" onClick={setClose}>X</button>
       <img src={details.userImageURL} alt="user profile picture" />
-      <h1 className="text-white">User: {details.user}</h1>
+      <h1 className="">User: {details.user}</h1>
       <img src={details.webformatURL}></img>
       <p>{details.tags}</p>
     </div>
