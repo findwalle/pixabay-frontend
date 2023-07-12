198f4e1adacc186415d1b9877152d128c63e60e0
Walter Li <106818226+findwalle@users.noreply.github.com>
2023-07-12 14:34:13 -0400

    update to imageModal - order tags as list

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -2,16 +2,19 @@ function ImageModal({ isOpen, setClose, details }) {
   if (!isOpen) {
     return null
   }
-  
   // TODO: address userImageURL not loading
 
   return (
     <div className="image-modal">
-      <button onClick={setClose}>X</button>
-      <img src={details.userImageURL} alt="user profile picture" />
-      <h1 className="">{details.user}</h1>
-      <img src={details.webformatURL}></img>
-      <p>{details.tags}</p>
+      <button className="modal-close-btn" onClick={setClose}>Back</button>
+      <img className="modal-user-pfp" src={details.userImageURL} alt="user profile picture" />
+      <h1 className="modal-username">{details.user}</h1>
+      <img className="modal-img" src={details.webformatURL}></img>
+      <ul className="modal-tags">
+        {details.tags.split(',').map((tag) => {
+          return <li>{tag}</li>
+        })}
+      </ul>
     </div>
   )
 }
